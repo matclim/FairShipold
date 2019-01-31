8171c6928bb2436e7e4827257d8ac6139fe75816
Matei Climescu <matei.climescu@cern.ch>
2019-01-31 08:33:37 +0100

    Commented Box.SetRunNumber in charmDetconf.py

--- python/charmDet_conf.py
+++ python/charmDet_conf.py
@@ -40,7 +40,7 @@ def configure(run,ship_geo):
  Box.SetCoatingParam(ship_geo.Box.CoatX, ship_geo.Box.CoatY, ship_geo.Box.CoatZ)
  Box.SetGapGeometry(ship_geo.Box.distancePassive2ECC)
  Box.SetTargetDesign(ship_geo.Box.Julytarget)
- Box.SetRunNumber(ship_geo.Box.RunNumber)
+ #Box.SetRunNumber(ship_geo.Box.RunNumber)
 
 
 #-----Pixel Detector-----
